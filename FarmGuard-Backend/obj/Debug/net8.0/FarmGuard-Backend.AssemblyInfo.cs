9ceb2c25231b7a30c6c1bc96c6ae26ded89af204
Software  Engineer Student | English | Spanish <102981505+Brays83@users.noreply.github.com>
2024-10-31 03:35:53 -0500

    fix[main]:fixed dockerfile

--- FarmGuard-Backend/obj/Debug/net8.0/FarmGuard-Backend.AssemblyInfo.cs
+++ FarmGuard-Backend/obj/Debug/net8.0/FarmGuard-Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FarmGuard-Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+759a0fddaedad991965caf2cc017457176213edb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb043dcf98843fa3a697c6ab1a1d213ccbe0fff0")]
 [assembly: System.Reflection.AssemblyProductAttribute("FarmGuard-Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FarmGuard-Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
